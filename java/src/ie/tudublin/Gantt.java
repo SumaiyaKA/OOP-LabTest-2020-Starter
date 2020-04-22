39b10b474f9b879a28d160295d8476f9511b20cb
Sumaiya <d17126680@mydit.ie>
2020-04-22 20:12:08 +0400

    Drawing the phases

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -55,14 +55,13 @@ public class Gantt extends PApplet
 
 	public void drawGrid() {
 
-		float border = width * 0.05f;
-		
-        // stroke(0, 0, 255); // set the colour used to draw lines and borders around shapes
+		float border = width * 0.05f; // border = 40
+
 		for(int i = 1; i <= 30; i++) 
 		{
-			// noFill();
+			
 			float l = map(i, 1, 30, border *4, width - border);
-			// String name = i.task;
+			
 			noStroke();
 			if( i%2 == 0 )
 			{
@@ -78,107 +77,36 @@ public class Gantt extends PApplet
 				rect(l, border, 1, height - (border*2) );
 			}
 
-			
 			// text(numbers, x, y)
 			fill(255);
 			text(i, l, border / 2); 
-
-
-			// text(names, x, y)
-			// text(name, border / 2, x);
-
-			/*
-
-			for(Task m:tasks)
-			{
-			
-				String task1 = m.task;
-				int start1 = m.start;
-				int end1 = m.end;
-	
-				System.out.println(task1 + ", " + start1 + ", " + end1);
-							
-			}
-
-            map(value, start1, stop1, start2, stop2)
-            float x = map(i, -5, 5, border, width - border);
-            line(x1, y1, x2, y2)
-            line(x, border, x, height - border);
-            line(border, x, width - border, x); 
-
-            fill(255); // white colour fill 
-            text(i, x, border / 2); // text(c, x, y)
-			text(i, border / 2, x);
-			*/
+		
         } 
 	}
 	
 	public void drawPhase() 
 	{
-		// for(Task m:tasks)
-		// colorMode(HSB);
+
 		float border = width * 0.05f;
 		
-        // stroke(0, 0, 255); // set the colour used to draw lines and borders around shapes
 		int i=0;
-		
 		for(Task w:tasks) 
 		{
-			// Task w = tasks.get(i);
-			// int phaseSize = tasks.size() - 1;
-			// float l = map(i, 1, 30, border *4, width - border);
-
-			// float daySize = 20.0f;
 			
-			float x = map (w.getStart(), 1, 30, border * 4, width -border);
-			float x1 = map (w.getEnd(), 1, 30, border *4, width - border);
-			float phaseSize = x1 - x;
-			// float phaseSize = (w.getEnd() - w.getStart() ) * daySize;
-			// float taskSize = map(w.getEnd()-w.getStart(), 0, 30, border*4, width - border );
-			// float x = map(i, 1, 30, border * 4 + (w.getStart() * daySize), width - border);
+			float startPoint = map (w.getStart(), 1, 30, border*4, width -border);
+			float endPoint = map (w.getEnd(), 1, 30, border*4, width - border);
+			float phaseSize = endPoint - startPoint;
+			
 			float y = map(i, 0, tasks.size(), border , height - (border*1) );
 
 			noStroke();
 			fill(i * w.getEnd(),255,255);
-			rect(x, y, phaseSize, 30);
+			rect(startPoint, y, phaseSize, 30);
 			fill(255);
 			textAlign(CENTER, CENTER);
 			text(w.getTask(), border*2 , y);
+
 			i++;
-
-			
-
-			// String name = i.task;
-
-			
-			// text(numbers, x, y)
-			// text(i, x, border / 2); 
-			// text(names, x, y)
-			// text(name, border / 2, x);
-
-			/*
-
-			for(Task m:tasks)
-			{
-			
-				String task1 = m.task;
-				int start1 = m.start;
-				int end1 = m.end;
-	
-				System.out.println(task1 + ", " + start1 + ", " + end1);
-							
-			}
-
-            map(value, start1, stop1, start2, stop2)
-            float x = map(i, -5, 5, border, width - border);
-            line(x1, y1, x2, y2)
-            line(x, border, x, height - border);
-            line(border, x, width - border, x); 
-
-            fill(255); // white colour fill 
-            text(i, x, border / 2); // text(c, x, y)
-			text(i, border / 2, x);
-			*/
         } 
         
     }
