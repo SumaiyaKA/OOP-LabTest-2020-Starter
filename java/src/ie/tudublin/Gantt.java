54d693c034cb461ed1385b6b3c9607f8c970bb47
Sumaiya <d17126680@mydit.ie>
2020-04-22 21:59:06 +0400

    modifying mousePress & mouseDrag

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -9,6 +9,9 @@ public class Gantt extends PApplet
 {	
 	// Declare an ArrayList to hold instances of the Task class:
 	public ArrayList<Task> tasks = new ArrayList<Task>();
+
+    int selected1 = -1;
+    int selected2 = -1;
 	
 	
 	public void settings()
@@ -49,8 +52,6 @@ public class Gantt extends PApplet
 		drawGrid();
 		drawPhase();
 		colorMode(HSB);
-		
-
 	}
 
 	public void drawGrid() {
@@ -104,24 +105,86 @@ public class Gantt extends PApplet
 			rect(startPoint, y, phaseSize, 30);
 			fill(255);
 			textAlign(CENTER, CENTER);
-			text(w.getTask(), border*2 , y);
+			text(w.getTask(), border*2 , y + 10);
 
 			i++;
         } 
         
-    }
+	}
 	
 	public void mousePressed()
-	{
-		println("Mouse pressed");	
+	{	
+		float rectArea = 20.0f;
+		float border = width * 0.05f;
+
+		for(int i = 0; i < tasks.size(); i++)
+        {
+			Task task = tasks.get(i);
+
+			float leftSide = map(task.getStart(), 1, 30, border*4, width - border);
+			float rightSide = map(task.getEnd(), 1, 30, border*4, width - border);
+
+            if (mouseX < (leftSide + rectArea) && mouseX > leftSide)
+            {
+				selected1 = i;
+			}
+			else if(mouseX > (rightSide - rectArea) && mouseX < rightSide)
+			{
+				selected2 = i;
+			}
+			
+        }
 	}
 
 	public void mouseDragged()
 	{
-		println("Mouse dragged");
-	}
+		int size;
+		float border = width * 0.05f;
 
-	
+		// Selected left of the box
+		if(selected1 != -1)
+		{
+			Task task = tasks.get(selected1);
+			float leftSide = map(task.getStart(), 1, 30, border*4, width - border);
+
+			// check if the left half of the rectangle had been moved to the left but not outside the grid
+			if(mouseX < leftSide && mouseX > border)
+			{
+				// reset the size depends on the mouse 
+				size = task.getStart() - 1;
+				task.setStart(size);
+			}
+			
+			// check if the left half of the rectangle had been moved to the right 
+			else if(task.getEnd() - task.getStart() != 1 && (mouseX > border*4) && mouseX > leftSide + 20)
+			{
+				// reset the size depends on the mouse movement
+				size = task.getStart() + 1;
+				task.setStart(size);
+			}
+		}
+		
+		else if(selected2 != -1)
+		{
+			Task task = tasks.get(selected2);
+
+			float rightSide = map(task.getEnd(), 1, 30, border*4, width - border);
+
+			// check if the right half of the rectangle had been moved to the left but not outside the grid
+			if( mouseX > rightSide && mouseX < (width - border) )
+			{
+				size = task.getEnd() + 1;
+				task.setEnd(size);
+			}
+			
+			// check if the left half of the rectangle had been moved to the right 
+			else if( (task.getEnd() - task.getStart() ) != 1 && mouseX < width - border)
+			{
+				size = task.getEnd() - 1;
+				task.setEnd(size);
+			}
+		}
+	}
 	
 	public void setup() 
 	{
