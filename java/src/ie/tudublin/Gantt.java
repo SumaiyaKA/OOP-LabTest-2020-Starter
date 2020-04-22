18c835fa8624372ee415d96b1c8d4ed65e078fa3
Sumaiya <d17126680@mydit.ie>
2020-04-22 13:12:30 +0400

    Drawing the grid with the numbers

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -43,6 +43,72 @@ public class Gantt extends PApplet
 		}
 		
 	}
+
+	// Write a method called displayTasks() that displays the tasks as in the video. 
+	// Each task should have a separate colour. 
+	// You can use HSB colour mode to achieve this. 
+	// To get full marks you should use the Processing map function in your solution
+	public void displayTasks()
+	{
+		drawGrid();
+
+	}
+
+	public void drawGrid() {
+
+		float border = width * 0.05f;
+		
+        // stroke(0, 0, 255); // set the colour used to draw lines and borders around shapes
+        textAlign(CENTER, CENTER);
+        for(int i = 1; i <= 30; i++) {
+
+			float x = map(i, 1, 30, border *4, width - border);
+			// String name = i.task;
+
+			if( i%2 == 0 )
+			{
+				stroke(255, 255, 255);
+				// line (x1, y1, x2, y2)
+				line(x, border, x, height - border);
+
+			}
+			else 
+			{
+				stroke(192, 192, 192);
+				line(x, border, x, height - border);
+			}
+
+			
+			// text(numbers, x, y)
+			text(i, x, border / 2); 
+			// text(names, x, y)
+			// text(name, border / 2, x);
+
+			/*
+
+			for(Task m:tasks)
+			{
+			
+				String task1 = m.task;
+				int start1 = m.start;
+				int end1 = m.end;
+	
+				System.out.println(task1 + ", " + start1 + ", " + end1);
+							
+			}
+
+            map(value, start1, stop1, start2, stop2)
+            float x = map(i, -5, 5, border, width - border);
+            line(x1, y1, x2, y2)
+            line(x, border, x, height - border);
+            line(border, x, width - border, x); 
+
+            fill(255); // white colour fill 
+            text(i, x, border / 2); // text(c, x, y)
+			text(i, border / 2, x);
+			*/
+        } 
+    }
 	
 	public void mousePressed()
 	{
@@ -65,5 +131,6 @@ public class Gantt extends PApplet
 	public void draw()
 	{			
 		background(0);
+		displayTasks();
 	}
 }
